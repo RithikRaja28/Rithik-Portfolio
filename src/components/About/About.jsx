f19540f2ea25d7dda4e09619c5ee398a06a76d27
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-05-25 10:07:25 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { ABOUT_TEXT } from '../../constants';
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -9,14 +9,14 @@ const About = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex items-center justify-center">
-            <img className='rounded-3xl' src="src\assets\rithikrajaprofilepic.jpg" alt="About" />
-          </div>
-          <div className="w-ful lg:w-1/2">
-            <div className="flex justify-center lg:justify-start">
-                <p className=''>{ABOUT_TEXT}</p>
-            </div>
+            <img className='rounded-2xl' src="src\assets\rithikrajaprofilepic.jpg" alt="About" />
           </div>
         </div>
+          <div className="w-full lg:w-1/2">
+            <div className="flex justify-center lg:justify-start">
+                <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
+            </div>
+          </div>
       </div>
     </div>
   );
