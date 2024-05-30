5ba671cf61131097b03472bbbe0b2f8afd140ac0
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-05-30 20:06:29 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
         transition = {{duration:0.5}}
-        className="flex flex-wrap"
+        className="flex flex-wrap "
       >
         <div className="w-full lg:w-1/2 lg:p-8 ">
           <div className="flex items-center justify-center">
