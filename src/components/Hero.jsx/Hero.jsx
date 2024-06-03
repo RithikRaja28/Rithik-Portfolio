c0620e387c453605b31c034a7c61568c2daf8bd2
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-03 20:16:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx/Hero.jsx
+++ src/components/Hero.jsx/Hero.jsx
@@ -45,6 +45,22 @@ const Hero = () => {
             >
               {HERO_CONTENT}
             </motion.p>
+            <motion.button
+              variants={container(1.5)}
+              initial="hidden"
+              animate="visible"
+              whileHover={{ scale: 1.05 }}
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+            >
+              <svg
+                class="fill-current w-4 h-4 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              <span>Download CV</span>
+            </motion.button>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
