41624538d6abccf2737cc686ddf4f97f016de5f6
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-05 22:23:04 +0530

    day commit

--- src/components/Hero.jsx/Hero.jsx
+++ src/components/Hero.jsx/Hero.jsx
@@ -3,6 +3,7 @@ import { HERO_CONTENT } from "../../constants";
 import profilePic from "../../assets/rithikrajaprofilepic.jpg";
 import { motion } from "framer-motion";
 import { FiDownload } from "react-icons/fi";
+import "./Hero.css";
 const container = (delay)=>({
   hidden:{x:-100,opacity:0},
   visible:{
@@ -49,10 +50,10 @@ const Hero = () => {
             {/* to be changed */}
 
             <motion.button
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="flex items-center gap-2 m-2 rounded-full border p-2"
+              variants={container(1.5)}
+              initial="hidden"
+              animate="visible"
+              class="download-button"
               onClick={() => {
                 const link = document.createElement("a");
                 link.href =
@@ -61,10 +62,43 @@ const Hero = () => {
                 link.click();
               }}
             >
-              <span>Download CV</span>
-              <span>
-                <FiDownload className="text-xl" />
-              </span>
+              <div class="docs">
+                <svg
+                  class="css-i6dzq1"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  fill="none"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  height="20"
+                  width="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                  <polyline points="14 2 14 8 20 8"></polyline>
+                  <line y2="13" x2="8" y1="13" x1="16"></line>
+                  <line y2="17" x2="8" y1="17" x1="16"></line>
+                  <polyline points="10 9 9 9 8 9"></polyline>
+                </svg>{" "}
+                Download CV
+              </div>
+              <div class="download">
+                <svg
+                  class="css-i6dzq1"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  fill="none"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  height="24"
+                  width="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                  <polyline points="7 10 12 15 17 10"></polyline>
+                  <line y2="3" x2="12" y1="15" x1="12"></line>
+                </svg>
+              </div>
             </motion.button>
           </div>
         </div>
